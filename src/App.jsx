b06d1caf4e45453039bf8dbf3a5d711a183d6b5e
Abdulloh1001 <leoncrow070@gmail.com>
2026-05-23 18:09:41 +0500

    props drilling oldi olindi

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
-import { useState, useEffect } from 'react';
 import Navbar from './components/navbar';
 import Card from './components/card';
-import { getUsers } from './service/api';
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [search, setSearch] = useState('');
-
-  useEffect(() => {
-    getUsers()
-      .then((data) => setUsers(data))
-      .catch((error) => console.error(error));
-  }, []);
-  
-  //Search function
-  const filterUsers = users.filter((user) =>
-    user.name
-    .toLowerCase()
-    .startsWith(search.toLowerCase())
-  );
-
   return (
     <div className='min-h-screen bg-gray-50 flex flex-col items-center w-full pb-10'>
         {/* Navbar */}
-        <Navbar search={search} setSearch={setSearch}/>
+        <Navbar />
         {/* Main content */}
-        <Card filterUsers={filterUsers} search={search} />
+        <Card />
     </div>
   );
 }
