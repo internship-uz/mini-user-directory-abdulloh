99922cf3205ae275b4d678182ee3c8be35b5f22a
Abdulloh1001 <leoncrow070@gmail.com>
2026-05-23 17:01:52 +0500

    API joylashtirildi

---
+++ src/service/api.js
@@ -0,0 +1,13 @@
+export const getUsers = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+};
